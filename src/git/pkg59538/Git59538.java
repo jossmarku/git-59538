b5ed2e915ed72e839b92ab5699fdfa03ca62d496
Personal <Personal@Familia>
2021-02-19 17:08:59 -0600

    Actividad 5.1 GitHub

---
+++ src/git/pkg59538/Git59538.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.pkg59538;
+
+/**
+ *
+ * @author Jossmar Oswaldo Ku Cupul
+ */
+public class Git59538 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nombre = "Jossmar Oswaldo";
+        String matricula = "59538";
+        System.out.println("Soy " + nombre + " y mi matrícula es " + matricula);
+    }
+    
+}
